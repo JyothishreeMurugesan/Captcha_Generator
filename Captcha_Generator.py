36bf8a00a319997dad49464b3a353d35b27f9e80
JyothishreeMurugesan <155998422+JyothishreeMurugesan@users.noreply.github.com>
2024-02-08 19:49:45 +0530

    Captcha_Generator.py

--- Captcha_Generator.py
+++ Captcha_Generator.py
@@ -2,12 +2,10 @@
 
 from random import choice
 
-A="ABCDEFGHIJKLMNOPQRSTUVWXYZ"
-a="abcdefghijklmnopqrstuvuxyz"
-Num = "0123456789"
+A="ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvuxy0123456789"
 
 captcha = " "
 for i in range(5):
-    captcha+=choice(A)+choice(a)+choice(Num)
+    captcha+=choice(A)
 
     print("Your captcha:",captcha)
